fd2c0d659765c5b2cd91ec8519b430e17d0027d1
stefanoghost <stefanoghost@gmail.com>
2026-04-21 12:03:14 +0200

    Delete role and rolebinding for stefano in project-dev

--- killercoda-scenarios/create-k8s-user-with-certificate/background.sh
+++ killercoda-scenarios/create-k8s-user-with-certificate/background.sh
@@ -5,7 +5,12 @@ echo "Preparing scenario..."
 kubectl config use-context kubernetes-admin@kubernetes >/dev/null 2>&1 || true
 
 kubectl delete clusterrolebinding stefano-view --ignore-not-found >/dev/null 2>&1 || true
+kubectl delete rolebinding stefano-dev-rb -n project-dev --ignore-not-found >/dev/null 2>&1 || true
+kubectl delete role stefano-dev-reader -n project-dev --ignore-not-found >/dev/null 2>&1 || true
 kubectl delete certificatesigningrequest stefano-certificate --ignore-not-found >/dev/null 2>&1 || true
+kubectl delete namespace project-dev --ignore-not-found >/dev/null 2>&1 || true
+
+kubectl create namespace project-dev >/dev/null 2>&1 || true
 
 rm -f /root/stefano.key /root/stefano.csr /root/stefano.crt
 rm -f /root/stefano.csr.base64
