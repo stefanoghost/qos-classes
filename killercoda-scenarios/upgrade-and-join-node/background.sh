ae61aa572718c267d3dfa1bf9b8e1ab5edc56f79
stefanoghost <stefanoghost@gmail.com>
2026-04-24 17:08:23 +0200

    Allow downgrades for kubeadm, kubelet, and kubectl

--- killercoda-scenarios/upgrade-and-join-node/background.sh
+++ killercoda-scenarios/upgrade-and-join-node/background.sh
@@ -4,7 +4,7 @@ echo "Setting up scenario..."
 
 ssh node01 << EOF
 sudo apt-get update -y
-sudo apt-get install -y kubeadm=1.35.0-1.1 kubelet=1.35.0-1.1 kubectl=1.35.0-1.1 || true 
+sudo apt-get install -y kubeadm=1.35.0-1.1 kubelet=1.35.0-1.1 kubectl=1.35.0-1.1 --allow-downgrades || true 
 sudo systemctl daemon-reexec
 sudo systemctl restart kubelet || true
 EOF
